173705775580614d41b52f69914a5fe52d6295ef
Lanrzip <lanrzip@outlook.com>
2020-08-18 13:38:25 +0800

    Initial commit

---
+++ MainDash.py
@@ -0,0 +1,98 @@
+import plotly_express as px
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+from WeatherSpider import *
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Graph(id='graph'),
+    dcc.Input(id='input_', type='text', value='北京'),
+    html.Button(id='submit', n_clicks=0, children='Submit'),
+    dcc.Interval(id='update', interval=86400000, n_intervals=0),
+    html.Div(id='intermediate-value')
+])
+
+
+def select_province(df, name):
+    return df.loc[df['省/直辖市'] == name]
+
+
+def main(df, name):
+    province = select_province(df, name)
+
+    if name == '广西':
+        name = '广西壮族自治区'
+    elif name == '内蒙古':
+        name = '内蒙古自治区'
+    elif name == '宁夏':
+        name = '宁夏回族自治区'
+    elif name == '西藏':
+        name = '西藏自治区'
+    elif name == '新疆':
+        name = '新疆维吾尔自治区'
+    elif name == '上海':
+        name = '上海市'
+    elif name == '重庆':
+        name = '重庆市'
+    elif name == '北京':
+        name = '北京市'
+    elif name == '天津':
+        name = '天津市'
+    else:
+        name += '省'
+
+    with open(f"city/{name}.json", 'r', encoding='utf-8') as fp:
+        geo = json.load(fp)
+
+    city = []
+    for c in geo['features']:
+        d = {}
+        d['name'] = c['properties']['name']
+        d['adcode'] = c['properties']['adcode']
+        city.append(d)
+
+    city_code = pd.DataFrame(city)
+
+    df = province.join(city_code.set_index('name'), on='城市')
+
+    fig = px.choropleth(df, geojson=geo, locations='adcode',
+                        featureidkey='properties.adcode', color='最高气温',
+                        hover_data=['城市', '最高气温', '最低气温'], title=f'{name}')
+    fig.update_geos(fitbounds="locations", visible=False)
+
+    return fig
+
+
+@app.callback(Output('intermediate-value','children'), [Input('update','n_intervals')])
+def update_data(n):
+    weather.job()
+    weather.send_message('济南', weather.city_list)
+
+    return "更新时间：{}".format(time.strftime("%Y-%m-%d %H:%M:%S",
+                                              time.localtime(weather.last_time)))
+
+
+@app.callback(Output('graph', 'figure'),
+              [Input('submit', 'n_clicks')],
+              [State('input_', 'value')])
+def update_output(n_clicks, input_):
+    time.sleep(2)
+    df = pd.DataFrame(weather.city_list)
+    df['城市'] = df['城市'].map(lambda x: x + '市')
+    index = df.query("`省/直辖市` in ['北京','上海','天津','重庆']")['城市'].index
+    df.loc[index,'城市'] = df.query("`省/直辖市` in ['北京','上海','天津','重庆']")['城市'].str.replace('市', '区')
+    df['最高气温'] = df['最高气温'].astype(float)
+    fig = main(df, input_)
+
+    return fig
+
+
+if __name__ == '__main__':
+    weather = Weather()
+    app.run_server()
---
+++ WeatherSpider.py
@@ -0,0 +1,91 @@
+import re
+import time
+import requests
+from lxml import etree
+from twilio.rest import Client
+import pandas as pd
+
+
+class Weather:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    }
+
+    def sub_blank(self, text):
+        return re.sub(r"\s", "", text)
+
+    def parse_url(self, url):
+        response = requests.get(url, headers=self.headers)
+        text = response.content.decode('utf-8')
+        html = etree.HTML(text)
+        day_list = html.xpath("//div[@class='hanml']/div")
+        tomorrow = day_list[1]
+        province_list = tomorrow.xpath("./div")
+        for province in province_list:
+            city = province.xpath(".//tr")
+            first = True
+            province = self.sub_blank("".join(city[2].xpath("./td[1]//text()")))
+            for tr in city[2:]:
+                detail = {}
+                tds = tr.xpath(".//td")
+
+                name = self.sub_blank("".join(tds[-8].xpath(".//text()")))
+                condition = tds[-7].xpath(".//text()")[0]
+                wind = self.sub_blank("".join(tds[-6].xpath(".//text()")))
+                high_temp = tds[-5].xpath(".//text()")[0]
+                condition_night = tds[-4].xpath(".//text()")[0]
+                wind_night = self.sub_blank("".join(tds[-3].xpath(".//text()")))
+                low_temp = tds[-2].xpath(".//text()")[0]
+                detail['省/直辖市'] = province
+                detail['城市'] = name
+                detail['白天天气'] = condition
+                detail['白天风况'] = wind
+                detail['最高气温'] = high_temp
+                detail['夜间天气'] = condition_night
+                detail['夜间风况'] = wind_night
+                detail['最低气温'] = low_temp
+                self.city_list.append(detail)
+                
+    def job(self):
+        self.city_list = []
+        self.last_time = time.time()
+        area_list = ['hb', 'db', 'hd', 'hz', 'hn', 'xb', 'xn']
+        for area in area_list:
+            self.parse_url(f"http://www.weather.com.cn/textFC/{area}.shtml")
+
+    def send_message(self, city, city_list):
+        df = pd.DataFrame(city_list)
+
+        city = city
+        cond = df.query("城市 == @city").squeeze().astype(str).to_list()
+        text = "\n" + cond[1] + "白天天气：" + cond[2] + " " + cond[3] + \
+               " " + "最高气温" + cond[4] + "°C" + "\n    " + \
+               "夜间天气：" + cond[5] + " " + cond[6] + \
+               " " + "最低气温" + cond[7] + "°C"
+
+        account = "AC7f63698bf6c852f6311153fad0c9f941"
+        token = "d0f18c508c25488b6b02e29db3f52500"
+        from_ = "+18569421593"
+        to = "+8618653195606"
+
+        client = Client(account, token)
+        message = client.messages.create(to=to,
+                                         from_=from_,
+                                         body=text)
+
+
+if __name__ == '__main__':
+
+    weather = Weather()
+    weather.job()
+    weather.send_message('济南', weather.city_list)
+    while True:
+        if time.time() - weather.last_time >= 86400:
+            weather.job()
+            print('最新爬取时间更新到{}'.format(time.strftime("%Y-%m-%d %H:%M:%S",
+                                                     time.localtime(weather.last_time))))
+
+            weather.send_message('济南', weather.city_list)
+
+        else:
+            time.sleep(1)
